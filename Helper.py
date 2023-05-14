29f64a261637c1c2f8145c263f8b5cfe553bc095
Wei Xuan <51687528+Jaywhisker@users.noreply.github.com>
2023-05-14 14:09:02 +0800

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -34,9 +34,17 @@ def insearch_result(search_term, title):
     return True
 
 
+#####################################################################
+# Main helper functions for sending prompts to GPT-3
+#####################################################################
+
 #function to tokenise and reduce length of prompt due to token limits
 #for openai we use a token limit of 2056 
 def tokenised(sentence, maxtoken):
+  try:
+    nltk.download('punkt')
+  except:
+    pass
   tokens = nltk.word_tokenize(sentence) #tokenise the sentence into tokens
   max_tokens = maxtoken # set the maximum number of tokens
 
@@ -44,5 +52,12 @@ def tokenised(sentence, maxtoken):
   truncated_tokens = tokens[:max_tokens] # select the first max_tokens tokens
   truncated_sentence = " ".join(truncated_tokens) # join the tokens back into a sentence
   return truncated_sentence
-  
-  
+
+
+#function to send prompt to model in openai, temperature at 0 such that the data is reproducible
+#prompt = tokenised(prompt) to ensure that prompt does not exceed limit
+def generate_texts(prompt, model): 
+    response = openai.Completion.create(
+        engine=model, prompt= tokenised(prompt, 2056), max_tokens=1024, n=10, stop=None, temperature=0.0
+    )
+    return response.choices[0].text
