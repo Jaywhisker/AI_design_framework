8ed1149627d8d6db6e68d352f004be6525ef67c2
Wei Xuan <51687528+Jaywhisker@users.noreply.github.com>
2023-05-14 17:21:49 +0800

    Update Transcript_Summariser.py

--- Generating_Design_Opportunities/Transcript_Summariser.py
+++ Generating_Design_Opportunities/Transcript_Summariser.py
@@ -10,18 +10,17 @@ from ...Helper import *
 
 #function to summarise one youtube transcript into its pros and cons
 #require: data is a singular paragraph containing the youtube transcript to summarise
-def single_transcript_summariser(data, search_terms, apikey):
+def single_transcript_summariser(data, search_terms, model = "text-davinci-003", apikey):
   openai.api_key = apikey
   prompt = "Summarise the flaws and strengths of the" + " ".join(search_terms) + ":" + data
-  model = "text-davinci-003" #GPT-3 model name
   summary = generate_text(prompt, model)
   return summary
 
 #function to summarise all youtube transcripts into its pros and cons
 #require: nested list of paragraph containing each youtube transcript
-def transcript_summariser(youtube_transcript, search_terms, apikey):
+def transcript_summariser(youtube_transcript, search_terms, model = "text-davinci-003", apikey):
   for transcripts in youtube_transcripts:
-  result = single_transcript_summariser(transcripts, search_terms, apikey)
+  result = single_transcript_summariser(transcripts, search_terms,model, apikey)
   summarised_reviews += result.strip() #remove whitespace 
   return summarised_reviews
 
