f83db2a177c70ee8215690fdb1cea2e0c42706c7
Wei Xuan <51687528+Jaywhisker@users.noreply.github.com>
2023-05-14 15:40:51 +0800

    Update Design_Opportunities.py

--- Generating_Design_Opportunities/Design_Opportunities.py
+++ Generating_Design_Opportunities/Design_Opportunities.py
@@ -11,15 +11,17 @@ from ...Helper import *
 #function to give suggestions on what to improve for each respective category with regards to the product 
 #require the keyword, the product name and data which is the list of negative comments related to the category of that product
 def suggestion_maker(data, keyword, search_term, apikey):
+  openai.api_key = apikey
   prompt = "based on the paragraph below, what is the best way to improve the" + " ".join(search_term) + " with regards to " + keyword + "only ?\n" + str(data)
   model = "text-davinci-003"
-  suggestion = generate_text(prompt, model,apikey)
+  suggestion = generate_text(prompt, model)
   return suggestion.strip()
 
 
 #function to state what should be maintained for each respective category with regards to the product
 #require the keyword, the product name and data which is the list of positive comments related to the category of that product
 def maintain_maker(data, keyword, search_term, apikey):
+  openai.api_key = apikey
   prompt = "based on the paragraph below, what is the best way to maintain in the" + " ".join(search_term) + " with regards to " + keyword + "only ?\n" + str(data)
   model = "text-davinci-003"
   maintainence = generate_text(prompt, model, apikey)
@@ -58,9 +60,10 @@ def generating_design_outcomes(negative_design_outcomes, positive_design_outcome
 #the prompt must mainly contain the reviews (containing things to maintain and suggestions for improvements) as well as prompts to compare with current product specifcations
 #after which you can save the file
 def finalised_design_outcomes(design_outcomes, search_terms, apikey):
+  openai.api_key = apikey
   prompt = "Imagine you are a product designer and these are the reviews you have received. Using the current " + " ".join(search_terms) + " specifications, provide a new set of product specifications with comparison to the current one to design an improved " + " ".join(search_terms) + " that meets the demands of the reviews. \n Reviews:" + str(design_outcomes)
   model = "text-davinci-003"
-  final_design_outcomes = generate_text(prompt, model, apikey)
+  final_design_outcomes = generate_text(prompt, model)
   return final_design_outcomes.strip()
 
 
